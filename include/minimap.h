96948269f6bfbabe590abd714ebd7873f5feecfc
revolveR99 <zamiri.abdulov7@gmail.com>
2024-01-25 09:09:39 +0100

    norm errors minimap.h fixed

--- include/minimap.h
+++ include/minimap.h
@@ -6,11 +6,11 @@
 /*   By: zabdulza <zabdulza@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 09:04:08 by zabdulza          #+#    #+#             */
-/*   Updated: 2024/01/25 09:04:10 by zabdulza         ###   ########.fr       */
+/*   Updated: 2024/01/25 09:08:58 by zabdulza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINIMAP_H  
+#ifndef MINIMAP_H
 # define MINIMAP_H
 
 # include "cub.h"
@@ -21,8 +21,5 @@
 # include "vector.h"
 # include <stdint.h>
 # include "game_struct.h"
-
-
-
 
 #endif
